077e0d62669fddedf02764afe2f138851510a2eb
evandro.abreu <evandro.abreu@digicon.com.br>
2025-02-17 08:25:08 -0300

    feat: updated icon

--- units-tool-wails/main.go
+++ units-tool-wails/main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"embed"
+  "embed"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+  "github.com/wailsapp/wails/v2"
+  "github.com/wailsapp/wails/v2/pkg/options"
+  "github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/src
 var assets embed.FS
 
 func main() {
-	app := NewApp()
+  app := NewApp()
 
-	err := wails.Run(&options.App{
-		Title:  "Configurador",
-		Width:  1440,
-		Height: 870,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
+  err := wails.Run(&options.App{
+    Title:  "Units Tool",
+    Width:  1440,
+    Height: 870,
+    AssetServer: &assetserver.Options{
+      Assets: assets,
+    },
+    BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+    OnStartup:        app.startup,
+    Bind: []interface{}{
+      app,
+    },
+  })
 
-	if err != nil {
-		println("Error:", err.Error())
-	}
+  if err != nil {
+    println("Error:", err.Error())
+  }
 }
